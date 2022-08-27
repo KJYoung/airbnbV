e4a0f3b0d3cf8fddcaec2fc55af24cf7d10f6498
KJYoung <jykim157@snu.ac.kr>
2022-08-27 19:44:32 +0900

    1.07 Before paginator(Manual Pagination) (11.3)

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 from datetime import datetime
 from . import models
@@ -6,16 +7,36 @@ from . import models
 
 
 def all_rooms(request):
-    # return HttpResponse(content="Hello")
-    now = datetime.now()
-    myName = "VKJYoung"
-    superMode = True
+    try:
+        pageNum = int(request.GET.get("page", 1))
+        if pageNum <= 0:
+            raise RuntimeError()
+    except:
+        pageNum = 1
+    try:
+        pageSize = int(request.GET.get("page_size", 10))
+        if pageSize <= 0:
+            raise RuntimeError()
+    except:
+        pageSize = 10
 
-    all_rooms = models.Room.objects.all()
+    offset = (pageNum - 1) * pageSize
+    limit = pageNum * pageSize
+
+    all_rooms = models.Room.objects.all()[offset:limit]
+
+    # Total page number calculation.
+    totalRoom = models.Room.objects.count()
+    pageTotal = ceil(totalRoom / pageSize)
+
     return render(
         request,
         "rooms/home.html",
         context={
             "rooms": all_rooms,
+            "pageNum": pageNum,
+            "pageTotal": pageTotal,
+            "pageRange": range(1, pageTotal + 1),
+            "pageSize": pageSize,
         },
     )
