855ea530a8c7903cf8564e192378a8b386239cf9
KJYoung <jykim157@snu.ac.kr>
2022-08-30 21:29:22 +0900

    1.095 Search Manuall Form (13.6)

--- rooms/views.py
+++ rooms/views.py
@@ -43,7 +43,7 @@ def room_detail(request, pk):
 
 def room_search(request):
     try:
-        city = request.GET.get("city", "Seoul")
+        city = request.GET.get("city", "Anywhere")
         city = str.capitalize(city)
         country = request.GET.get("country", "KR")
         room_type = int(request.GET.get("room_types", 0))
@@ -52,8 +52,8 @@ def room_search(request):
         beds = int(request.GET.get("beds", 0))
         bedrooms = int(request.GET.get("bedrooms", 0))
         baths = int(request.GET.get("baths", 0))
-        instant = request.GET.get("instant", False)
-        superhost = request.GET.get("superhost", False)
+        instant = bool(request.GET.get("instant", False))
+        superhost = bool(request.GET.get("superhost", False))
 
         s_amenities = request.GET.getlist("amenities")
         s_facilities = request.GET.getlist("facilities")
@@ -82,10 +82,45 @@ def room_search(request):
             "amenities": amenities,
             "facilities": facilities,
         }
+
+        rooms = models.Room.objects.all()
+        filter_args = {}
+
+        if city != "Anywhere":
+            filter_args["city__startswith"] = city
+        filter_args["country"] = country
+        if room_type != 0:
+            filter_args["room_type__pk__exact"] = room_type
+        if price != 0:
+            filter_args["price__lte"] = price
+        if guests != 0:
+            filter_args["guests__gte"] = guests
+        if beds != 0:
+            filter_args["beds__gte"] = beds
+        if bedrooms != 0:
+            filter_args["bedrooms__gte"] = bedrooms
+        if baths != 0:
+            filter_args["baths__gte"] = baths
+        if instant:
+            filter_args["instant_book"] = True
+        if superhost:
+            filter_args["host__superhost"] = True
+        if len(s_amenities) > 0:
+            for selected_amen_id in s_amenities:
+                # filter_args["amenities__pk"] = int(selected_amen_id)
+                rooms = rooms.filter(amenities__pk=int(selected_amen_id))
+
+        if len(s_facilities) > 0:
+            for selected_fcil_id in s_facilities:
+                # filter_args["facilities__pk"] = int(selected_fcil_id)
+                rooms = rooms.filter(facilities__pk=int(selected_fcil_id))
+
+        rooms = rooms.filter(**filter_args)
+
         return render(
             request,
             "rooms/room_search.html",
-            {**form, **choices},
+            {**form, **choices, "rooms": rooms},
         )
     except:
         raise Http404()  # raise not return.
