7a272beb6bc12014daa37f2f540a29377e03ac97
KJYoung <jykim157@snu.ac.kr>
2022-09-03 23:19:11 +0900

    1.15 Setup TailwindCSS (19.1)

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from django.views.generic import FormView
 from django.core.files.base import ContentFile
+from django.contrib.auth.forms import UserCreationForm
 from . import forms, models
 
 
@@ -53,6 +54,7 @@ def logout_view(request):
 class SignUpView(FormView):
     template_name: str = "users/signup.html"
     form_class = forms.SignUpForm
+    # form_class = UserCreationForm
     success_url = reverse_lazy("core:home")
     initial = {
         "first_name": "Junyoung",
